b58dc55b5adaaf545dc3ee9ec6cecf5423574b0b
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-07-12 21:19:16 +0900

    set project

---
+++ src/test/kotlin/com/assign/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assign.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

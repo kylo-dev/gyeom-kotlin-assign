171b479f86206f8f1e06b8c1953cf3a1a13b70bb
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-07-12 22:19:15 +0900

    feature: UserEntity, BaseTimeEntity 추가

---
+++ src/main/kotlin/com/assign/backend/domain/user/entity/UserEntity.kt
@@ -0,0 +1,22 @@
+package com.assign.backend.domain.user.entity
+
+import com.assign.backend.domain.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val email: String,
+    val password: String,
+    val name: String,
+    @Enumerated(EnumType.STRING)
+    val role: Role,
+): BaseTimeEntity() {
+}
+
+enum class Role {
+    MEMBER, ADMIN
+}
